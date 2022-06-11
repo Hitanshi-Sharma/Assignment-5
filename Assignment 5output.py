0b758dd8afdac09fa6c11f79a09f1e2fb20332f7
Hitanshi-Sharma <103354645+Hitanshi-Sharma@users.noreply.github.com>
2022-06-11 11:58:12 +0530

    Add files via upload

---
+++ Assignment 5output.py
@@ -0,0 +1,138 @@
+C:\Users\dogra\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/dogra/AppData/Roaming/JetBrains/PyCharmCE2022.1/scratches/scratch_7.py
+Enter a string : hitanshi 
+Reversed string is  ihsnatih 
+Enter the range of numbers: 1 100
+Enter number: 8
+8
+16
+24
+32
+40
+48
+56
+64
+72
+80
+88
+96
+Enter first side of the triangle: 4
+Enter second side of the triangle: 5
+Enter third side of the triangle: 6
+Area of the triangle is 9.921567416492215
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+Enter the no of rows: 7
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYZAB
+
+Enter the range of numbers: 1 200
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+103
+107
+109
+113
+127
+131
+137
+139
+149
+151
+157
+163
+167
+173
+179
+181
+191
+193
+197
+199
+
+77
+154
+231
+308
+385
+462
+
+Enter 1 number: 2
+Enter 2 number: 3
+Enter 3 number: 20
+Enter 4 number: 10
+Enter 5 number: 7
+Enter 6 number: 9
+Enter 7 number: 19
+Enter 8 number: 14
+Enter 9 number: 45
+Enter 10 number: 23
+[2, 3, 20, 10, 7, 9, 19, 14, 45, 23]
+Positive numbers are: 
+2
+3
+20
+10
+7
+9
+19
+14
+45
+23
+Negative numbers are: 
+Odd numbers are: 
+3
+7
+9
+19
+45
+23
+Even numbers are: 
+2
+20
+10
+14
+No of times each number occurs in the List:
+{2: 1, 3: 1, 20: 1, 10: 1, 7: 1, 9: 1, 19: 1, 14: 1, 45: 1, 23: 1}
+
+Enter a string: it is raining heavily 
+{'it': 1, 'is': 1, 'raining': 1, 'heavily': 1, '': 1}
+
+Process finished with exit code 0
